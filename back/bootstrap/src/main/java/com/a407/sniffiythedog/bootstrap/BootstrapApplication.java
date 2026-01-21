5400973d183b274f53b29cd3a7381b488576b1c4
길태환 <fnfnfn258@gmail.com>
2026-01-21 09:50:48 +0900

    fix: runable

--- back/bootstrap/src/main/java/com/a407/sniffiythedog/bootstrap/BootstrapApplication.java
+++ back/bootstrap/src/main/java/com/a407/sniffiythedog/bootstrap/BootstrapApplication.java
@@ -2,8 +2,13 @@ package com.a407.sniffiythedog.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.a407.sniffiythedog"
+})
 public class BootstrapApplication {
 
 	public static void main(String[] args) {
