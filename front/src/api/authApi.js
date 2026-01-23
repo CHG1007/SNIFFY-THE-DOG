463eeeac67dd7396c32fa6c1d35d7289f1032570
최홍권 <hkhk6880@gmail.com>
2026-01-23 17:07:36 +0900

    feat(auth): 카카오 로그인 UI 및 백엔드 연동 추가

---
+++ front/src/api/authApi.js
@@ -0,0 +1,10 @@
+﻿import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "",
+});
+
+export const loginWithKakao = (token) =>
+  apiClient.post("/api/v1/auth/kakao", {
+    AccessToken: token,
+  });
