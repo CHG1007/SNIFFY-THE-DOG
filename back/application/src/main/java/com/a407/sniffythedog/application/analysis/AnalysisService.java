c4b84ecd59c9bd34dc3e1bbd556595d801dfba82
bjybjy1120 <bjybjy1120@naver.com>
2026-01-26 16:39:58 +0900

    feat: response, request 추가

--- back/application/src/main/java/com/a407/sniffythedog/application/analysis/AnalysisService.java
+++ back/application/src/main/java/com/a407/sniffythedog/application/analysis/AnalysisService.java
@@ -1,5 +1,6 @@
 package com.a407.sniffythedog.application.analysis;
 
+import com.a407.sniffythedog.application.analysis.in.AnalysisResult;
 import com.a407.sniffythedog.application.analysis.out.GmsPort;
 import com.a407.sniffythedog.domain.analysis.entity.AnalysisNarrative;
 import com.a407.sniffythedog.domain.analysis.enums.AiModel;
@@ -13,11 +14,8 @@ import java.util.Map;
 public class AnalysisService {
     private final GmsPort gmsPort;
 
-    public AnalysisNarrative analyze(Map<String, Object> payload) {
-        // GMS로부터 분석된 텍스트 수신
+    public AnalysisResult analyze(Map<String, Object> payload) {
         String text = gmsPort.analyze(AiModel.GPT_5_NANO, payload);
-
-        // 프론트엔드에 전달할 객체 생성
-        return new AnalysisNarrative(text, System.currentTimeMillis());
+        return new AnalysisResult(text, System.currentTimeMillis());
     }
 }
