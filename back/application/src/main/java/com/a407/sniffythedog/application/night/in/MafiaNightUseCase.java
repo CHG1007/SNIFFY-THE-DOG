ea5e6ea4fd7e3816db752644dc381ae7fbe892b1
gcy9293 <gcy9293@naver.com>
2026-01-28 09:08:14 +0900

    feat : night paze controller 구현

---
+++ back/application/src/main/java/com/a407/sniffythedog/application/night/in/MafiaNightUseCase.java
@@ -0,0 +1,5 @@
+package com.a407.sniffythedog.application.night.in;
+
+public interface MafiaNightUseCase {
+    void selectMafia(String roomCode, long mafiaUserId, long targetUserId);
+}
---
+++ back/application/src/main/java/com/a407/sniffythedog/application/night/in/NightResolveUseCase.java
@@ -0,0 +1,5 @@
+package com.a407.sniffythedog.application.night.in;
+
+public interface NightResolveUseCase {
+    void resolve(String roomCode);
+}
