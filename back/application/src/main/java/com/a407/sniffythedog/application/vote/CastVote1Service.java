135d84f39868e675b8c14a9e9c31a1e4ee5579d5
gcy9293 <gcy9293@naver.com>
2026-01-29 15:24:28 +0900

    fix : 투표 1차 2차 service redis 저장

--- back/application/src/main/java/com/a407/sniffythedog/application/vote/CastVote1Service.java
+++ back/application/src/main/java/com/a407/sniffythedog/application/vote/CastVote1Service.java
@@ -2,6 +2,7 @@ package com.a407.sniffythedog.application.vote;
 
 import com.a407.sniffythedog.application.common.exception.ApplicationException;
 import com.a407.sniffythedog.application.common.exception.ExceptionType;
+import com.a407.sniffythedog.application.gamelog.out.GameLogRedisPort;
 import com.a407.sniffythedog.application.room.out.RedisRoomPort;
 import com.a407.sniffythedog.application.vote.in.CastVote1Command;
 import com.a407.sniffythedog.application.vote.in.CastVote1UseCase;
@@ -13,6 +14,7 @@ import com.a407.sniffythedog.domain.game.enums.RoomStatus;
 import com.a407.sniffythedog.domain.game.vo.GameUserId;
 import com.a407.sniffythedog.domain.game.vo.RoomId;
 import com.a407.sniffythedog.domain.game.vo.VoteState;
+import com.a407.sniffythedog.domain.gamelog.vo.GameEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,7 @@ public class CastVote1Service implements CastVote1UseCase {
 
     private final RedisRoomPort redisRoomPort; // 방 상태 저장
     private final RoomEventPort roomEventPort; // 투표 여부를 알리는 포트
+    private final GameLogRedisPort gameLogRedisPort;
 
     @Override
     public void execute(CastVote1Command command) {
@@ -63,6 +66,8 @@ public class CastVote1Service implements CastVote1UseCase {
             // 4. 투표 반영 하기 호호,, 코드 안에 메서드로 (touch()로 version++ 자동)
             room.castDayVote(voterId, targetId);
 
+            vote1ResultHolder.round = 1; //todo: round 변경 해야함 ..
+
             // 5. 전원 투표 완료면 집계해서 accused 설정
             long aliveCount = room.getPlayers().values().stream().filter(PlayerState::isAlive).count(); // 투표해야하는 사람 수
             int totalVotes = room.getGameState().dayVote().getTotalVotes(); // 이미 투표한 사람 수
@@ -76,6 +81,7 @@ public class CastVote1Service implements CastVote1UseCase {
                         .filter(e -> e.getValue() == max)
                         .map(Map.Entry::getKey)
                         .toList();
+
 
                 // 단독 1등
                 if (top.size() == 1) {
@@ -103,12 +109,27 @@ public class CastVote1Service implements CastVote1UseCase {
                     vote1ResultHolder.isTie
             );
         }
+
+        gameLogRedisPort.saveEvent(command.roomCode(),
+                GameEvent.vote1Cast(vote1ResultHolder.round, command.voterUserId(), command.targetUserId())
+        );
+
+        if (vote1ResultHolder.resolved()) {
+            gameLogRedisPort.saveEvent(command.roomCode(),
+                    GameEvent.vote1Result(vote1ResultHolder.round, vote1ResultHolder.accusedUserId, vote1ResultHolder.isTie)
+            );
+        }
+
     }
 
     // updateRoomAtomically 내부에서 계산된 결과를 바깥으로 들고 나오기 위한 간단 홀더
     private static class Holder {
+        int round = 1; //todo: round 1로 해놔서 이거 바꿔야함
+
         Long accusedUserId = null;
+
         boolean isTie = false;
+
         boolean resolved()
          { return isTie || accusedUserId != null; }
     }
