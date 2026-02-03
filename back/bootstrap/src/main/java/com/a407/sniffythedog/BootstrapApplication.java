617206f99f3e82166ed72e396c21023b7c464a4c
박연준 <129067071+QUWR@users.noreply.github.com>
2026-02-03 17:04:26 +0900

    Update BootstrapApplication.java

--- back/bootstrap/src/main/java/com/a407/sniffythedog/BootstrapApplication.java
+++ back/bootstrap/src/main/java/com/a407/sniffythedog/BootstrapApplication.java
@@ -3,9 +3,11 @@ package com.a407.sniffythedog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.a407.sniffythedog", exclude = {
 		UserDetailsServiceAutoConfiguration.class })
+@EnableScheduling
 public class BootstrapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapApplication.class, args);
